18c8a15790fc2c4acb3f3ff41d7ea6588bc5f0c4
kangareuben <reubenbrenneradams@gmail.com>
2025-02-16 10:00:23 -0500

    Trying async, though I think this is the same thing with slightly different syntax

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import Bot from "./lib/bot.js";
 import getPostText from "./lib/getPostText.js";
 
-const text = await Bot.run(getPostText);
+const text = async () => {
+	await Bot.run(getPostText);
+};
 
 console.log(`[${new Date().toISOString()}] Posted: "${text}"`);
